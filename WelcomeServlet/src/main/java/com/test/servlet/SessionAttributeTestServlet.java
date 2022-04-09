57afeaaa8fb4f5262a5dcd5d057f5b779db5a913
Tamilselvan <tamilselvan.coder@gmail.com>
2022-04-09 19:20:16 +0530

    Added Servlet session context listener code

---
+++ WelcomeServlet/src/main/java/com/test/servlet/SessionAttributeTestServlet.java
@@ -0,0 +1,33 @@
+package com.test.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Enumeration;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+@WebServlet(name = "SessionAttributeTestServlet", description = "Servlet with Annotation", urlPatterns = {
+		"/SessionAttributeTestServlet" })
+public class SessionAttributeTestServlet extends HttpServlet {
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		HttpSession session = request.getSession();
+		String attributeName = "SessionAttribute";
+		String attributeValue = "Initial Value";
+		session.setAttribute(attributeName, attributeValue);
+		attributeValue = "New Value";
+		session.setAttribute(attributeName, attributeValue);
+		session.removeAttribute(attributeName);
+	}
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		doGet(req, resp);
+	}
+}
---
+++ WelcomeServlet/src/main/java/com/test/servlet/TestWebServletAnnotationListener.java
@@ -0,0 +1,41 @@
+package com.test.servlet;
+
+import javax.servlet.annotation.WebListener;
+import javax.servlet.http.HttpSessionAttributeListener;
+import javax.servlet.http.HttpSessionBindingEvent;
+
+@WebListener
+public class TestWebServletAnnotationListener implements HttpSessionAttributeListener {
+	/**
+	 * Default constructor.
+	 */
+	public TestWebServletAnnotationListener() {
+	}
+
+	@Override
+	public void attributeAdded(HttpSessionBindingEvent httpSessionBindingEvent) {
+		System.out.println("Attribute has been added");
+		String attributeName = httpSessionBindingEvent.getName();
+		Object attributeValue = httpSessionBindingEvent.getValue();
+		System.out.println("Attribute Name ::" + attributeName);
+		System.out.println("Attribute Value ::" + attributeValue.toString());
+	}
+
+	@Override
+	public void attributeRemoved(HttpSessionBindingEvent httpSessionBindingEvent) {
+		System.out.println("Attribute has been Removed");
+		String attributeName = httpSessionBindingEvent.getName();
+		Object attributeValue = httpSessionBindingEvent.getValue();
+		System.out.println("Attribute Name ::" + attributeName);
+		System.out.println("Attribute Value ::" + attributeValue.toString());
+	}
+
+	@Override
+	public void attributeReplaced(HttpSessionBindingEvent httpSessionBindingEvent) {
+		System.out.println("Attribute has been Replaced");
+		String attributeName = httpSessionBindingEvent.getName();
+		Object attributeValue = httpSessionBindingEvent.getValue();
+		System.out.println("Attribute Name ::" + attributeName);
+		System.out.println("Attribute Value ::" + attributeValue.toString());
+	}
+}
