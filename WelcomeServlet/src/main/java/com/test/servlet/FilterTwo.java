221214ee363a936ce3f6e43a29220f6d6c47f30f
Tamilselvan <tamilselvan.coder@gmail.com>
2022-04-09 18:53:27 +0530

    Added Servlet filter code

---
+++ WelcomeServlet/src/main/java/com/test/servlet/FilterTwo.java
@@ -0,0 +1,28 @@
+package com.test.servlet;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+
+@WebFilter(filterName = "filterTwo", urlPatterns = "/msg/*", servletNames = "WelcomeServlet")
+public class FilterTwo implements Filter {
+	@Override
+	public void init(FilterConfig filterConfig) throws ServletException {
+	}
+
+	@Override
+	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
+			throws IOException, ServletException {
+		System.out.println("Inside filter two.");
+		chain.doFilter(request, response);
+	}
+
+	@Override
+	public void destroy() {
+	}
+}
