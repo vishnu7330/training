a79c03981bba034e63401cbe350079980040f233
Tamilselvan <tamilselvan.coder@gmail.com>
2022-04-10 17:05:59 +0530

    Webservice client for testing

---
+++ WebserviceTest/src/com/training/web/service/package-info.java
@@ -0,0 +1,2 @@
+@javax.xml.bind.annotation.XmlSchema(namespace = "http://service.web.training.com", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
+package com.training.web.service;
