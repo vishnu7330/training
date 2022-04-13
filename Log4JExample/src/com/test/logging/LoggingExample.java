0a289f4aadf82163cd73b8f10cc5f2ceeddf098b
Tamilselvan <tamilselvan.coder@gmail.com>
2022-04-13 15:56:24 +0530

    added comments

--- Log4JExample/src/com/test/logging/LoggingExample.java
+++ Log4JExample/src/com/test/logging/LoggingExample.java
@@ -10,8 +10,13 @@ public class LoggingExample {
 
 	public static void main(String[] args) {
 		
+		//used for debugging
 		logger.debug("Debug log message");
+		
+		//used for info messages
 		logger.info("Info log message");
+		
+		//used for error conditions
 		logger.error("Error log message");
 	}
 
