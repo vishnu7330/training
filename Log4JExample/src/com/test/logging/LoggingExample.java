a694004f95076ad4a67242836405f28dc0d203f3
Tamilselvan <tamilselvan.coder@gmail.com>
2022-04-15 08:31:13 +0530

    log4j log levels added

--- Log4JExample/src/com/test/logging/LoggingExample.java
+++ Log4JExample/src/com/test/logging/LoggingExample.java
@@ -10,14 +10,23 @@ public class LoggingExample {
 
 	public static void main(String[] args) {
 		
+		//Trace used to provide lot of information
+		logger.trace("Trace log message");
+		
 		//used for debugging
 		logger.debug("Debug log message");
 		
 		//used for info messages
 		logger.info("Info log message");
 		
+		//used for warning messages
+		logger.warn("Warn log message");
+		
 		//used for error conditions
 		logger.error("Error log message");
+		
+		//used for fatal conditions
+		logger.fatal("Fatal log message");
 	}
 
 }
