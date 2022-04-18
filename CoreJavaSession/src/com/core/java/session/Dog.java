4e5be597b742c9d5c0a9ff30ac3902c4701f4aad
Tamilselvan <tamilselvan.coder@gmail.com>
2022-04-18 09:45:27 +0530

    added comments

--- CoreJavaSession/src/com/core/java/session/Dog.java
+++ CoreJavaSession/src/com/core/java/session/Dog.java
@@ -1,6 +1,7 @@
 package com.core.java.session;
 
 /* method overriding */
+//child class
 public class Dog extends Animal {
 
 	public Dog() {
@@ -15,12 +16,16 @@ public class Dog extends Animal {
 	public Dog(String name) {
 		super(name);
 	}
-
+	//overwritten method
 	public void animalSound() {
 		System.out.println("The dog says: bow wow");
 	}
 	public void animalSound(int age) {
 		System.out.println("The dog says: bow wow, my age is " + age);
+	}
+	
+	public void bark() {
+		System.out.println("I'm loud");
 	}
 
 }
--- CoreJavaSession/src/com/core/java/session/Pig.java
+++ CoreJavaSession/src/com/core/java/session/Pig.java
@@ -1,9 +1,14 @@
 package com.core.java.session;
 
 /* method overriding */
+//child class
 public class Pig extends Animal {
 	
 	public void animalSound() {
 		System.out.println("The pig says: wee wee");
+	}
+	
+	public void run() {
+		System.out.println("I'm running");
 	}
 }
