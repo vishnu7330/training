fd5ad5c4399581576bb807a0fbe5a7bb5fa17798
Tamilselvan <tamilselvan.coder@gmail.com>
2022-04-20 16:30:15 +0530

    Updated address for one to many relationships

--- StrutsSpringHibernateExample/src/main/java/com/training/struts/spring/hibernate/service/EmployeeManagerImpl.java
+++ StrutsSpringHibernateExample/src/main/java/com/training/struts/spring/hibernate/service/EmployeeManagerImpl.java
@@ -10,7 +10,7 @@ import com.training.struts.spring.hibernate.model.Address;
 import com.training.struts.spring.hibernate.model.Employee;
 
 public class EmployeeManagerImpl implements EmployeeManager {
-	
+
 	// Employee dao injected by Spring context
 	private EmployeeDAO employeeDAO;
 
@@ -18,20 +18,47 @@ public class EmployeeManagerImpl implements EmployeeManager {
 	@Override
 	@Transactional
 	public void addEmployee(Employee employee) {
+
 		List<Address> addresses = new ArrayList<Address>();
-		Address address = new Address();
-		address.setCity("SFO");
-		address.setState("CA");
-		address.setStreet("Green park");
-		address.setZipCode("12345");
-		addresses.add(address);
-		
-		//add all addresses to the employee list
+
+		// address 1
+		Address address1 = new Address();
+		address1.setStreet("615, Park Street");
+		address1.setCity("DES MOINES");
+		address1.setState("Iowa");
+		address1.setZipCode("50309");
+		addresses.add(address1);
+
+		// address 2
+		Address address2 = new Address();
+		address2.setStreet("34655 SKYLARK DR APT 626");
+		address2.setCity("UNION CITY");
+		address2.setState("California");
+		address2.setZipCode("94587");
+		addresses.add(address2);
+
+		// address 3
+		Address address3 = new Address();
+		address3.setStreet("362 W Olive Ave Apt 9");
+		address3.setCity("SUNNYVALE");
+		address3.setState("California");
+		address3.setZipCode("94086");
+		addresses.add(address3);
+
+		// address 4
+		Address address4 = new Address();
+		address4.setStreet("11316 JOLLYVILLE RD APT 115");
+		address4.setCity("AUSTIN");
+		address4.setState("Texas");
+		address4.setZipCode("78759");
+		addresses.add(address4);
+
+		// add all addresses to the employee list
 		employee.setAddressList(addresses);
-		
-		//for Join column value
-		address.setEmployee(employee);
-		
+
+		// for Join column value
+		address1.setEmployee(employee);
+
 		employeeDAO.addEmployee(employee);
 	}
 
