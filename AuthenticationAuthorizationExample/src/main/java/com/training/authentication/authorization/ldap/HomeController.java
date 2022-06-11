2cd81a110d3fdf28b8e7c07987777ec1a4d4aec5
Tamilselvan <tamilselvan.coder@gmail.com>
2022-06-11 18:39:24 +0530

    uncommented home controller

--- AuthenticationAuthorizationExample/src/main/java/com/training/authentication/authorization/ldap/HomeController.java
+++ AuthenticationAuthorizationExample/src/main/java/com/training/authentication/authorization/ldap/HomeController.java
@@ -1,18 +1,13 @@
-//package com.training.authentication.authorization.ldap;
-//
-//import org.springframework.web.bind.annotation.GetMapping;
-//import org.springframework.web.bind.annotation.RestController;
-//
-//@RestController
-//public class HomeController {
-//
-//	@GetMapping("/admin")
-//	public String admin() {
-//		return "Welcome to the Admin Panel!";
-//	}
-//
-//	@GetMapping("/home")
-//	public String home() {
-//		return "Welcome to the Home page!";
-//	}
-//}
+package com.training.authentication.authorization.ldap;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+	@GetMapping("/home")
+	public String home() {
+		return "Welcome to the Home page!";
+	}
+}
