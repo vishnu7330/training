9e984c688b2d1ac2d1f34c10a5ba31038af5d209
Tamilselvan <tamilselvan.coder@gmail.com>
2022-07-01 14:40:53 +0530

    Removed test project

--- WebserviceTest/src/com/training/web/service/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "http://service.web.training.com", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package com.training.web.service;
