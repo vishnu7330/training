ca1c34fa83c3d77638a41256730e7617392a8f59
Tamilselvan <tamilselvan.coder@gmail.com>
2022-07-01 14:22:44 +0530

    test commit

--- AuthenticationAuthorizationExample/src/main/java/com/training/authentication/authorization/LdapAuthenticationAuthorizationExampleApplication.java
+++ AuthenticationAuthorizationExample/src/main/java/com/training/authentication/authorization/LdapAuthenticationAuthorizationExampleApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class LdapAuthenticationAuthorizationExampleApplication {
 
+	
 	public static void main(String[] args) {
 		SpringApplication.run(LdapAuthenticationAuthorizationExampleApplication.class, args);
 	}
