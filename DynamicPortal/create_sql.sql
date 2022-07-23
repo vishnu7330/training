ad09070daf7ae5f9540505d00cb612a73423bd39
Tamilselvan <tamilselvan.coder@gmail.com>
2022-07-23 12:13:55 +0530

    added mysql table data scripts

--- DynamicPortal/create_sql.sql
+++ DynamicPortal/create_sql.sql
@@ -32,3 +32,22 @@ CREATE TABLE `portal`.`departments` (
   `id` varchar(10) NOT NULL,
   `name` varchar(100) NOT NULL,
   PRIMARY KEY (`id`));
+  
+  
+-- data scripts
+-- STUDENT Table
+INSERT INTO `portal`.`student`(`id`,`name`,`rollno`) VALUES('1' ,'N Pratik','22CSE567');
+
+--SUBJECT table
+INSERT INTO `portal`.`subject`(`id`,`name`) VALUES('1','Programming in C');
+INSERT INTO `portal`.`subject`(`id`,`name`) VALUES('2','Programming in C++');
+INSERT INTO `portal`.`subject`(`id`,`name`) VALUES('3','Programming in Java');
+INSERT INTO `portal`.`subject`(`id`,`name`) VALUES('4','Engineering Mathematics-1');
+INSERT INTO `portal`.`subject`(`id`,`name`) VALUES('5','Professional Communication');
+
+--SCORE table
+INSERT INTO `portal`.`score` (`id`,`student_id`,`subject_id`,`score`) VALUES('1','1','1','98');
+INSERT INTO `portal`.`score` (`id`,`student_id`,`subject_id`,`score`) VALUES('2','1','2','87');
+INSERT INTO `portal`.`score` (`id`,`student_id`,`subject_id`,`score`) VALUES('3','1','3','97');
+INSERT INTO `portal`.`score` (`id`,`student_id`,`subject_id`,`score`) VALUES('4','1','4','92');
+INSERT INTO `portal`.`score` (`id`,`student_id`,`subject_id`,`score`) VALUES('5','1','5','86');
