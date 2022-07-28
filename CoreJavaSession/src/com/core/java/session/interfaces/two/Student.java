e977841a1c364d4aab773d093b134f530ea8b7bd
Tamilselvan <tamilselvan.coder@gmail.com>
2022-07-28 08:54:14 +0530

    added another type of Interface example

---
+++ CoreJavaSession/src/com/core/java/session/interfaces/two/Student.java
@@ -0,0 +1,7 @@
+package com.core.java.session.interfaces.two;
+
+public interface Student {
+
+	public String getName();
+	
+}
---
+++ CoreJavaSession/src/com/core/java/session/interfaces/two/StudentImpl.java
@@ -0,0 +1,10 @@
+package com.core.java.session.interfaces.two;
+
+public class StudentImpl implements Student {
+
+	@Override
+	public String getName() {
+		return "John Joseph";
+	}
+
+}
