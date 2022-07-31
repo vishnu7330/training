5e137a819c87a198f6815586c6de2b2746958732
Tamilselvan <tamilselvan.coder@gmail.com>
2022-07-31 10:31:40 +0530

    added zoned date time example

--- CoreJavaSession/src/com/core/java/session/DateAndTime.java
+++ CoreJavaSession/src/com/core/java/session/DateAndTime.java
@@ -1,10 +1,15 @@
 package com.core.java.session;
 
 import java.time.Duration;
+import java.time.Instant;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.Month;
+import java.time.ZoneId;
+import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
 
 public class DateAndTime {
 
@@ -38,14 +43,57 @@ public class DateAndTime {
 //		java.util.Date
 //		java.util.Calendar
 
+		ZonedDateTime zdt = null;
+		Instant instant = Instant.now();
+		DateTimeFormatter formatter = DateTimeFormatter.ISO_TIME;
+		System.out.println("+++++++++++++++++++++++++++++++++++");
+
+		// Eastern time
+		String est = ZoneOffset.SHORT_IDS.get("EST");
+		zdt = instant.atZone(ZoneId.of(est));
+		System.out.println("EST TIME:*************** " + formatter.format(zdt));
+
+		zdt = instant.atZone(ZoneId.of("US/Eastern"));
+		System.out.println("EST TIME: " + formatter.format(zdt));
+		// Eastern time - another way
+		zdt = ZonedDateTime.now(ZoneId.of("UTC-04:00"));
+		System.out.println("UTC-04:00 -> " + zdt);
+		System.out.println("EST TIME: " + formatter.format(zdt));
+		System.out.println("+++++++++++++++++++++++++++++++++++");
+
+		String pst = ZoneOffset.SHORT_IDS.get("PST");
+		zdt = instant.atZone(ZoneId.of(pst));
+		System.out.println("PST : " + zdt);
+		System.out.println("PST TIME: " + formatter.format(zdt));
+		System.out.println("+++++++++++++++++++++++++++++++++++");
+
+		// Central time
+		String cst = ZoneOffset.SHORT_IDS.get("CST");
+		zdt = instant.atZone(ZoneId.of(cst));
+		System.out.println("CST : " + zdt);
+		System.out.println("CST TIME: " + formatter.format(zdt));
+		System.out.println("+++++++++++++++++++++++++++++++++++");
+		// Central time - another way
+		ZonedDateTime utc = ZonedDateTime.now(ZoneId.of("UTC-05:00"));
+		System.out.println("UTC-05:00 -> " + utc);
+
+		// India time
+		String ist = ZoneOffset.SHORT_IDS.get("IST");
+		zdt = instant.atZone(ZoneId.of(ist));
+		System.out.println("IST : " + zdt);
+		System.out.println("IST TIME: " + formatter.format(zdt));
+		System.out.println("+++++++++++++++++++++++++++++++++++");
+		// India time - another way
+		ZonedDateTime gmt = ZonedDateTime.now(ZoneId.of("GMT+05:30"));
+		System.out.println("GMT+05:30 -> " + gmt);
+
 	}
 
 	private static void pause() {
 		try {
-			//milliseconds
+			// milliseconds
 			Thread.sleep(5000);
 		} catch (InterruptedException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
