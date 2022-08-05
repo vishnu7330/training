c5fd84809fbb0602ea48c27974d15ea8a16821e7
Tamilselvan <tamilselvan.coder@gmail.com>
2022-08-05 11:50:15 +0530

    Micro services - initial commit for Hystrix Dashboard

---
+++ micro-hystrix-dashboard/src/main/java/com/training/micro/hystrix/dashboard/MicroHystrixDashboardApplication.java
@@ -0,0 +1,13 @@
+package com.training.micro.hystrix.dashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroHystrixDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroHystrixDashboardApplication.class, args);
+	}
+
+}
