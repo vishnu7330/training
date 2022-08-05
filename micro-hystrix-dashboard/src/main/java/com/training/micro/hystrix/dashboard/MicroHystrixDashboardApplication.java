edf40a11b97c8f5fa45bc10f0d82141969bd36f4
Tamilselvan <tamilselvan.coder@gmail.com>
2022-08-05 11:58:30 +0530

    added Hystrix Dashboard configuration

--- micro-hystrix-dashboard/src/main/java/com/training/micro/hystrix/dashboard/MicroHystrixDashboardApplication.java
+++ micro-hystrix-dashboard/src/main/java/com/training/micro/hystrix/dashboard/MicroHystrixDashboardApplication.java
@@ -2,8 +2,12 @@ package com.training.micro.hystrix.dashboard;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
 
 @SpringBootApplication
+@EnableEurekaClient
+@EnableHystrixDashboard
 public class MicroHystrixDashboardApplication {
 
 	public static void main(String[] args) {
