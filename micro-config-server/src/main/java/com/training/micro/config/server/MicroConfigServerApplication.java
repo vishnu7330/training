917230542b3943565d2e9f70a40ffaddb2c8af30
Tamilselvan <tamilselvan.coder@gmail.com>
2022-08-05 14:28:09 +0530

    Initial commit for Micro services config server

---
+++ micro-config-server/src/main/java/com/training/micro/config/server/MicroConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.training.micro.config.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroConfigServerApplication.class, args);
+	}
+
+}
---
+++ micro-config-server/src/test/java/com/training/micro/config/server/MicroConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.training.micro.config.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
