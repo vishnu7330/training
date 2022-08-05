e834ff5ef4e6e9964887c6ea03842564e608d1f3
Tamilselvan <tamilselvan.coder@gmail.com>
2022-08-05 09:41:17 +0530

    initial commit for micro services api gateway

---
+++ micro-api-gateway/src/main/java/com/training/micro/api/gateway/MicroApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.training.micro.api.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroApiGatewayApplication.class, args);
+	}
+
+}
