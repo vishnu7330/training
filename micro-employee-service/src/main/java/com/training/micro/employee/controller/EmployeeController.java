a595d98503d541c0c38097d5038d34628ffcd6e5
Tamilselvan <tamilselvan.coder@gmail.com>
2022-08-09 17:54:02 +0530

    added log4j2 logging to employee micro service

--- micro-employee-service/src/main/java/com/training/micro/employee/controller/EmployeeController.java
+++ micro-employee-service/src/main/java/com/training/micro/employee/controller/EmployeeController.java
@@ -1,5 +1,7 @@
 package com.training.micro.employee.controller;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,16 +18,20 @@ import com.training.micro.employee.service.EmployeeService;
 @RequestMapping("/employees")
 public class EmployeeController {
 
+	Logger log = LogManager.getLogger(EmployeeController.class);
+
 	@Autowired
 	EmployeeService employeeService;
 
 	@PostMapping("/")
 	public Employee addEmployee(@RequestBody Employee employee) {
+		log.info("inside addEmployee()");
 		return employeeService.addEmployee(employee);
 	}
 
 	@GetMapping("/{id}")
 	public ResponseTemplate getEmployeeWithDepartment(@PathVariable("id") Long employeeId) {
+		log.info("inside getEmployeeWithDepartment()");
 		return employeeService.getEmployeeWithDepartment(employeeId);
 	}
 
