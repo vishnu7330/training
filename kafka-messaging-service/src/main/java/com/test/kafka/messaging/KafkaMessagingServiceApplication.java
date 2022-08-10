079be2296f9e673779fbe757ef70b929cd54dfa4
Tamilselvan <tamilselvan.coder@gmail.com>
2022-08-10 17:42:27 +0530

    Kafka messaging service initial commit

---
+++ kafka-messaging-service/src/main/java/com/test/kafka/messaging/KafkaMessagingServiceApplication.java
@@ -0,0 +1,35 @@
+package com.test.kafka.messaging;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.core.KafkaTemplate;
+
+@SpringBootApplication
+public class KafkaMessagingServiceApplication implements ApplicationRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KafkaMessagingServiceApplication.class, args);
+	}
+
+	@Autowired
+	private KafkaTemplate<String, String> kafkaTemplate;
+
+	public void sendMessage(String msg) {
+		kafkaTemplate.send("weatherreport", msg);
+	}
+
+	@KafkaListener(topics = "weatherreport", groupId = "group-id")
+	public void listen(String message) {
+		System.out.println("Received Messasge in group - group-id: " + message);
+	}
+
+	@Override
+	public void run(ApplicationArguments args) throws Exception {
+		sendMessage("Weather is 40 degrees");
+	}
+
+}
---
+++ kafka-messaging-service/src/test/java/com/test/kafka/messaging/KafkaMessagingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.kafka.messaging;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaMessagingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
