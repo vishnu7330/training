ea2786bd7d96c979665552f2b23be451e225646f
Tamilselvan <tamilselvan.coder@gmail.com>
2022-08-15 18:29:05 +0530

    added JavascriptExample project

---
+++ JavascriptExample/src/main/webapp/app.js
@@ -0,0 +1,34 @@
+function msg() {
+
+	alert("Welcome to Javascript");
+}
+
+
+function changeContent() {
+
+	document.getElementById("mycontent").innerHTML = "Welcome to Javascript";
+
+}
+
+function changeContentWithJquery() {
+
+	$('#mycontent').text("Welcome to Jquery");
+
+}
+
+function changeContentWithAjax() {
+	$.ajax('api/message/data.txt',   // request url
+		{
+			success: function(data, status, xhr) {    // success callback function
+				$('#mycontent').text(data);
+			}
+		});
+}
+
+
+$(document).ready(function() {
+
+	alert("on load");
+	$('#mycontent').text("Loading...");
+
+});
