8e6758b29ddfa515b309405f83c7b0c53e096872
Tamilselvan <tamilselvan.coder@gmail.com>
2022-08-28 10:21:40 +0530

    Getting logged in username from Authentication information

--- ExternalLdapServerExample/src/main/java/com/example/external/ldap/controller/HomeController.java
+++ ExternalLdapServerExample/src/main/java/com/example/external/ldap/controller/HomeController.java
@@ -1,5 +1,7 @@
 package com.example.external.ldap.controller;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,9 +10,13 @@ public class HomeController {
 
 	@GetMapping("/hello")
 	public String hello() {
-		return "Hello";
+		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+		String currentPrincipalName = authentication.getName();
+		System.out.println("Authorities: " + authentication.getAuthorities());
+		System.out.println("Details: " + authentication.getDetails());
+		return "Hello " + currentPrincipalName;
 	}
-	
+
 	@GetMapping("/admin")
 	public String admin() {
 		return "Admin";
