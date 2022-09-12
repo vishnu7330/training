e2dfdf86b1beb6886c8c8731382ddf7d2370be12
Tamilselvan <tamilselvan.coder@gmail.com>
2022-09-12 12:02:07 +0530

    deleted kafka-messaging-service project

--- kafka-messaging-service/start-kafka.bat
+++
@@ -1,6 +0,0 @@
-set JAVA_HOME=%ProgramFiles%\Java\jdk1.8.0_321
-start zookeeper-server-start.bat ..\..\config\zookeeper.properties
- 
-TIMEOUT 10
- 
-start kafka-server-start.bat ..\..\config\server.properties
