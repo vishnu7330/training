1dfbb10b35edfdd8edc2dba8f042af8759961f0e
Tamilselvan <tamilselvan.coder@gmail.com>
2022-10-10 09:26:49 +0530

    added example classes for Datatypes

--- CoreJavaSession/src/com/core/java/session/DatatypesExamples.java
+++ CoreJavaSession/src/com/core/java/session/DatatypesExamples.java
@@ -91,3 +91,99 @@ class Policy {
 		this.totalPremium = totalPremium;
 	}
 }
+
+// Banking industry/domain example 
+class AccountDetails {
+	
+	private long accountNumber;
+	private String name;
+	private String accountType;
+	private long balance;
+	private Address address;
+
+	public long getAccountNumber() {
+		return accountNumber;
+	}
+
+	public void setAccountNumber(long accountNumber) {
+		this.accountNumber = accountNumber;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAccountType() {
+		return accountType;
+	}
+
+	public void setAccountType(String accountType) {
+		this.accountType = accountType;
+	}
+
+	public long getBalance() {
+		return balance;
+	}
+
+	public void setBalance(long balance) {
+		this.balance = balance;
+	}
+
+	public Address getAddress() {
+		return address;
+	}
+
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+}
+
+class Address {
+
+	private String street;
+
+	private String city;
+
+	private String state;
+
+	private String zipCode;
+
+	public String getStreet() {
+		return street;
+	}
+
+	public void setStreet(String street) {
+		this.street = street;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public String getZipCode() {
+		return zipCode;
+	}
+
+	public void setZipCode(String zipCode) {
+		this.zipCode = zipCode;
+	}
+}
+
+
+
