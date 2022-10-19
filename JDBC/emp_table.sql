4226cdbe734321ce7f7e8657b3a1ab36c5202026
Tamilselvan <tamilselvan.coder@gmail.com>
2022-10-19 22:36:06 +0530

    added emp table creation sql

---
+++ JDBC/emp_table.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `emp` (
+  `PersonID` int DEFAULT NULL,
+  `LastName` varchar(255) DEFAULT NULL,
+  `FirstName` varchar(255) DEFAULT NULL,
+  `Address` varchar(255) DEFAULT NULL,
+  `City` varchar(255) DEFAULT NULL
+)
