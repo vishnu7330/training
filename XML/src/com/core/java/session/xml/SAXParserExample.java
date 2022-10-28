13a5cdbdebebd081dd2912fe1fb7bf7568276ac7
Tamilselvan <tamilselvan.coder@gmail.com>
2022-10-28 17:28:51 +0530

    Added SAX parser example

---
+++ XML/src/com/core/java/session/xml/SAXParserExample.java
@@ -0,0 +1,36 @@
+package com.core.java.session.xml;
+
+import java.io.File;
+import java.util.List;
+
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+
+public class SAXParserExample {
+
+	public static void main(String[] args) {
+
+		System.out.println("\n****** SAX Parser example *******\n");
+
+		try {
+
+			SAXParserFactory factory = SAXParserFactory.newInstance();
+			SAXParser saxParser = factory.newSAXParser();
+
+			// custom class *** NOT from SAX parser api
+			SAXParserHandler handler = new SAXParserHandler();
+
+			// input file
+			File input = new File("resources\\input.xml");
+			saxParser.parse(input, handler);
+
+			// print all
+			List<Student> result = handler.getResult();
+			result.forEach(System.out::println);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
---
+++ XML/src/com/core/java/session/xml/SAXParserHandler.java
@@ -0,0 +1,75 @@
+package com.core.java.session.xml;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.xml.sax.Attributes;
+import org.xml.sax.helpers.DefaultHandler;
+
+public class SAXParserHandler extends DefaultHandler {
+
+	private StringBuilder currentValue = new StringBuilder();
+
+	List<Student> result;
+	Student currentStudent;
+
+	public List<Student> getResult() {
+		return result;
+	}
+
+	@Override
+	public void startDocument() {
+		result = new ArrayList<Student>();
+	}
+
+	@Override
+	public void startElement(String uri, String localName, String qName, Attributes attributes) {
+
+		// reset the tag value
+		currentValue.setLength(0);
+
+		// start of loop
+		if (qName.equalsIgnoreCase("student")) {
+
+			// new student
+			currentStudent = new Student();
+
+			// student rollno
+			String rollno = attributes.getValue("rollno");
+			currentStudent.setRollno(rollno);
+		}
+	}
+
+	@Override
+	public void endElement(String uri, String localName, String qName) {
+
+		if (qName.equalsIgnoreCase("firstname")) {
+			currentStudent.setFirstname(currentValue.toString());
+		}
+
+		if (qName.equalsIgnoreCase("lastname")) {
+			currentStudent.setLastname(currentValue.toString());
+		}
+
+		if (qName.equalsIgnoreCase("nickname")) {
+			currentStudent.setNickname(currentValue.toString());
+		}
+
+		if (qName.equalsIgnoreCase("marks")) {
+			currentStudent.setMarks(Integer.parseInt(currentValue.toString()));
+		}
+
+		// end of loop
+		if (qName.equalsIgnoreCase("student")) {
+			result.add(currentStudent);
+		}
+
+	}
+
+	@Override
+	public void characters(char ch[], int start, int length) {
+		currentValue.append(ch, start, length);
+
+	}
+
+}
