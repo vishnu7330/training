571a69e05170d0ecc249d03aead20341d91b4b03
Tamilselvan <tamilselvan.coder@gmail.com>
2022-11-19 06:27:23 +0530

    formatting changes

--- CoreJavaSession/src/com/core/java/session/ConditionalStatements.java
+++ CoreJavaSession/src/com/core/java/session/ConditionalStatements.java
@@ -8,7 +8,7 @@ public class ConditionalStatements {
 
 		/** if condition **/
 		if (x < 10) {
-			System.out.println("inside ** if ** condition");
+			System.out.println("inside ** first if ** condition");
 		}
 
 		/** if-else condition **/
@@ -21,33 +21,33 @@ public class ConditionalStatements {
 
 		/** switch-case condition **/
 
-		int number = 10;
+		int number = 2;
 
 		switch (number) {
 
-		case 1: {
-			System.out.println("inside case 1");
-		}
-		case 2: {
-			System.out.println("inside case 2");
-		}
-		case 3: {
-			System.out.println("inside case 3");
-		}
-		case 4: {
-			System.out.println("inside case 4");
-		}
-		case 5: {
-			System.out.println("inside case 5");
-			/** break stops the flow of execution **/
-			break;
-		}
-		case 6: {
-			System.out.println("inside case 6");
-		}
-		/** when no case is having a matching value then default is executed **/
-		default:
-			System.out.println("inside default");
+			case 1: {
+				System.out.println("inside case 1");
+			}
+			case 2: {
+				System.out.println("inside case 2");
+			}
+			case 3: {
+				System.out.println("inside case 3");
+			}
+			case 4: {
+				System.out.println("inside case 4");
+			}
+			case 5: {
+				System.out.println("inside case 5");
+				/** break stops the flow of execution **/
+				break;
+			}
+			case 6: {
+				System.out.println("inside case 6");
+			}
+			/** when no case is having a matching value then default is executed **/
+			default:
+				System.out.println("inside default");
 
 		}
 
@@ -80,14 +80,14 @@ public class ConditionalStatements {
 			i++;
 		}
 
-		i = 0;
+		i = 10;
 		do {
 			System.out.println("inside do-while loop i -> " + i);
 			i++;
 		} while (i < 5);
 		
 		/** switch with return statement **/
-		String day = getDay(4);
+		String day = getDay(8);
 		System.out.println("Day: " + day);
 	}
 	
@@ -96,21 +96,21 @@ public class ConditionalStatements {
 		
 		/** switch with return statement **/
 		switch(number) {
-		case 1:
-			return "Sunday";
-		case 2:
-			return "Monday";
-		case 3:
-			return "Tuesday";
-		case 4:
-			return "Wednesday";
-		case 5:
-			return "Thursday";
-		case 6:
-			return "Friday";
-		case 7:
-			return "Saturday";
-
+			case 1:
+				return "Sunday";
+			case 2:
+				return "Monday";
+			case 3:
+				return "Tuesday";
+			case 4:
+				return "Wednesday";
+			case 5:
+				return "Thursday";
+			case 6:
+				return "Friday";
+			case 7:
+				return "Saturday";
+	
 			default:
 				return "Invalid number";
 		}
