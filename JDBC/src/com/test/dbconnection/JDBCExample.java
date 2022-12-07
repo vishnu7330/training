ee87f73d85bb9c64622f6091f9716115b7297bff
Tamilselvan <tamilselvan.coder@gmail.com>
2022-12-07 07:32:44 +0530

    updated database table name

--- JDBC/src/com/test/dbconnection/JDBCExample.java
+++ JDBC/src/com/test/dbconnection/JDBCExample.java
@@ -26,7 +26,7 @@ public class JDBCExample {
 			// ************ Using Statement *********//
 			Statement stmt = con.createStatement();
 
-			ResultSet rs = stmt.executeQuery("select * from emp");
+			ResultSet rs = stmt.executeQuery("select * from employee");
 			while (rs.next()) {
 				System.out.println(rs.getInt(1) + "  " + rs.getString(2) + "  " + rs.getString(3));
 			}
